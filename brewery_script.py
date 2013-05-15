0e26835eee9b5af80ddcd8cd962ef0559a0ddf37
Carl Reynolds <drcjar@gmail.com>
2013-05-15 14:37:13 +0100

    code for dissertation

---
+++ brewery_script.py
@@ -0,0 +1,13 @@
+import brewery
+from sys import argv
+
+script, inputfilename = argv
+
+b = brewery.create_builder()
+b.csv_source("%s" % inputfilename)
+b.audit()
+
+b.pretty_printer()
+
+b.stream.run()
+
